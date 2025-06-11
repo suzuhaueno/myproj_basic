d60224a5b27637a933a060bd15c0381c24d5d5aa
okuya-aoto <aoto_okuya@sanotec.co.jp>
2025-06-11 15:21:44 +0900

    19行目改行分追記。

--- src/jp/co/f1/basic/ch02/FieldVariable.java
+++ src/jp/co/f1/basic/ch02/FieldVariable.java
@@ -8,15 +8,15 @@ class Computer2 {
 public class FieldVariable {
 
 	public static void main(String[] args) {
-		
+
 		Computer2 com = new Computer2();
-		
+
 		com.os ="windowsXP";
 		com.memory = 2048;
-		
+
 		System.out.println("パソコンのOSは「" + com.os + "」です。");
 		System.out.println("メモリのサイズは「" + com.memory + "MByte」です。");
-
+		System.out.println();
 	}
 
 }
